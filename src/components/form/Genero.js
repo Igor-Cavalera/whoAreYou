c43387cd4656c88ff7123e39810d747d5018e2ca
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-03-29 16:36:30 -0300

    adicionando-opcoes-input-genero

--- src/components/form/Genero.js
+++ src/components/form/Genero.js
@@ -1,6 +1,13 @@
 function Genero () {
     return  (
-        <input type="text" className="genero" placeholder="Gênero" />
+        <>
+            <input type="text" className="genero" placeholder="Gênero" maxLength={15} list="generos" />
+            <datalist id="generos">
+                <option value="Masculino">Masculino</option>
+                <option value="Feminino">Feminino</option>
+                <option value="Não-Binário">Não-Binário</option>
+            </datalist>
+        </>
     )
 
 }
