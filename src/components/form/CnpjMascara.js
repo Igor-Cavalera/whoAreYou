3470ce20319f150a7595c67512a08c86e1519463
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-04-13 11:45:50 -0300

    configurando-mascara-cnpj

--- src/components/form/CnpjMascara.js
+++ src/components/form/CnpjMascara.js
@@ -1,5 +1,28 @@
-function CnpjMascara () {
-    return 
+import InputMask from 'react-input-mask';
+
+const onlyNumbers = (str) => str.replace(/[^0-9]/g,'');
+
+const CnpjMascara = ({value, onChange}) => {
+    function handleChange (event) {
+        onChange({...event,
+        target: {
+            ...event.target,
+            value: onlyNumbers(event.target.value)
+        }})
+        
+    }
+    return (
+        <>
+            <InputMask 
+                mask="99.999.999/9999-99" 
+                maskChar="" 
+                className="cnpj" 
+                placeholder="CNPJ" 
+                value={value} 
+                onChange={onChange}
+            />
+        </>
+    )
 }
 
 export default CnpjMascara;
