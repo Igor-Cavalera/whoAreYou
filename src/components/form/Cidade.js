8137205305be93808ce89cf3fccb8a1eba593da6
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-04-15 03:18:04 -0300

    corrigir-seletor-cidade

--- src/components/form/Cidade.js
+++ src/components/form/Cidade.js
@@ -1,8 +1,40 @@
+import {useEffect, useState} from "react"
+import axios from "axios";
+import EstadoUf from "./EstadoUf";
+
 function Cidade () {
+
+    <EstadoUf/>
+    const [cities, setCities] = useState([]);
+    const [selectedUf, setSelectedUf] = useState("0");
+
+    useEffect(() => {
+        axios
+        .get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response => {
+            setCities(response.data)
+        })
+    },[selectedUf])
+
+    function handleSelectedUf (event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+
     return (
         <>
-            <select name="cidade" id="cidade" className="cidade">
+            <select 
+                name="cidade" 
+                id="cidade" 
+                className="cidade"
+                onChange={ handleSelectedUf }
+            >
                 <option value="0">Cidade</option>
+                {cities.map(city => {
+                    <option key={city.id} value={city.nome}>
+                        {city.nome}
+                    </option>
+                })}
             </select>
         </>
     )
