9480aea8fc60fd3d71118da159dea54b60d4aeca
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-04-21 07:54:46 -0300

    organizando-aba-home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,22 +3,8 @@ function Home() {
     return (
         <>
             <div className="home">
-                <h1>Who are you?</h1>
+                <h1>WHO ARE YOU?</h1>
             </div>
-            <div className="fotoTest">
-                <h3>Foto</h3>
-            </div>
-        <div className="valoresResumo">
-            <div>
-               <h3> Valor do Nome Completo </h3>
-            </div>
-            <div>
-               <h3> Valor do CPF </h3>
-            </div>
-            <div>
-                <h3>Valor da Data de Nascimento</h3>
-            </div>
-        </div>
         </>
     )
 }
